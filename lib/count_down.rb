8d21d15ea41e1c1a1a018c1ae84f91beccedd5eb
Oscar A Castro <oacastro2@gmail.com>
2020-08-10 19:56:12 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,7 @@
-count += 1
-while count < 3 do 
+magic_exit_number = 7
+count = 0
+while count < 10 do 
+  break if count == magic_exit_number 
   puts "I am the #{count}, I love to count!"
-end  
-
+    count = count + 1 
+end    
