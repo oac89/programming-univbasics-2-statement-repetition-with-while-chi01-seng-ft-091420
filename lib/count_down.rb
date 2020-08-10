e6fdce0138d2edc5ca19dc32d38a5e49924c52ed
Oscar A Castro <oacastro2@gmail.com>
2020-08-10 20:11:36 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,21 +3,8 @@ while (condition) do
 end
 
 count = 10 
-while count >= 1 do
-  
-  count -= 1
-end
-
-count = 10 
-while count >= 1 do 
-  puts count
-  count -= 1 
-end
-
-count = 10 
 while count >= 1 do 
   puts count
   count -= 1 
 end
 puts "Happy New Year!"
-
